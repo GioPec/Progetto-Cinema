3f78cf903739adff17250ed9436e4ad45fb70150
Giovanni Pecorelli <48129769+GioPec@users.noreply.github.com>
2020-05-04 19:14:18 +0200

    inizio login

---
+++ ScriptLog.js
@@ -0,0 +1,7 @@
+function validaForm(){
+    if(document.myForm.remember.checked){
+        window.alert("Hai scelto di ricordarti per i prossimi accessi");
+    }else{
+        window.alert("Hai scelto di non ricordarti per i prossimi accessi");
+    }
+}
---
+++ login.php
@@ -0,0 +1,35 @@
+<html>
+    <head></head>
+    <body>
+        <?php
+            $dbconn= pg_connect("host=localhost port=5432 dbname=CinemaMarcoPolo user=postgres password=admin")
+            or die('Could not connect: ' . pg_last_error());
+            if(!(isset($_POST['loginButton']))){
+                header("Location: homepage.html");
+            }else{
+                $email=$_POST['inputEmail'];
+                $q1="select * from utenti where username=$1";
+                $result=pg_query_params($dbconn, $q1, array($email));
+                if(!($line=pg_fetch_array($result, null, PGSQL_ASSOC))){
+                    echo "<h1> Sorry you are not a registered user </h1>
+                    <a href=registrazione.html>
+                    Click here to register
+                    </a>";
+                }else{
+                    $password = ($_POST['inputPassword']);  //md5
+                    $q2="select * from utenti where username=$1 and password=$2";
+                    $result=pg_query_params($dbconn,$q2, array($email,$password));
+                    if(!($line=pg_fetch_array($result,null, PGSQL_ASSOC))){
+                        echo "<h1> The password is wrong! Grrrr</h1>
+                        <a href=login.html>Click here to login
+                        </a>";
+                    }else{
+                        $nome=$line['nome'];
+                        echo "<a href=../Welcome.php?name=$nome> Premi qui
+                        </a> per iniziare ad utilizzare il sito web";
+                    }
+                }
+            }
+        ?>
+    </body>
+</html>
